41d6b0c797664da5ccc45fbde5b2618e8aafb4ad
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-01-13 11:08:39 +0900

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,14 @@
 JAVA_OPTS="-server $JAVA_OPTS"
 JAVA_OPTS="-Xms3072m $JAVA_OPTS"
 JAVA_OPTS="-Xmx3072m $JAVA_OPTS"
-JAVA_OPTS="-XX:PermSize=256m $JAVA_OPTS"
-JAVA_OPTS="-XX:MaxPermSize=256m $JAVA_OPTS"
+
+# For JDK 7
+# JAVA_OPTS="-XX:PermSize=256m $JAVA_OPTS"
+# JAVA_OPTS="-XX:MaxPermSize=256m $JAVA_OPTS"
+# For JDK 8
+JAVA_OPTS="-XX:MetaspaceSize=256m $JAVA_OPTS"
+JAVA_OPTS="-XX:MaxMetaspaceSize=256m $JAVA_OPTS"
+
 JAVA_OPTS="-Xloggc:$CATALINA_HOME/logs/gc.log $JAVA_OPTS"
 JAVA_OPTS="-XX:+UseConcMarkSweepGC $JAVA_OPTS"
 JAVA_OPTS="-XX:+UseCMSCompactAtFullCollection $JAVA_OPTS"
