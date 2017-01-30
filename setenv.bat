2ff02ab0a76066d6404944c3962eb611e00e94ff
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-01-30 18:08:41 +0900

    Update setenv.bat

--- setenv.bat
+++ setenv.bat
@@ -1,20 +1,34 @@
-set JAVA_OPTS=%JAVA_OPTS% ^
--Xms2048m ^
--Xmx2048m ^
--XX:PermSize=256m ^
--XX:MaxPermSize=256m ^
--Xloggc:%CATALINA_HOME%/logs/gc.log ^
--XX:+UseConcMarkSweepGC ^
--XX:+UseCMSCompactAtFullCollection ^
--XX:+CMSClassUnloadingEnabled ^
--XX:+PrintHeapAtGC ^
--XX:+PrintGCDetails ^
--XX:+PrintGCDateStamps ^
--XX:+UseGCLogFileRotation ^
--XX:NumberOfGCLogFiles=5 ^
--XX:GCLogFileSize=10M ^
--XX:+HeapDumpOnOutOfMemoryError ^
--XX:HeapDumpPath=%CATALINA_HOME%/logs/ ^
--XX:ErrorFile=%CATALINA_HOME%/logs/hs_err_pid%p.log ^
--server 
+set JAVA_OPTS=%JAVA_OPTS% -server 
+set JAVA_OPTS=%JAVA_OPTS% -Xms2048m
+set JAVA_OPTS=%JAVA_OPTS% -Xmx2048m
+
+REM For JDK 7
+REM set JAVA_OPTS=%JAVA_OPTS% -XX:PermSize=256m
+REM set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256m
+REM For JDK 8
+set JAVA_OPTS=%JAVA_OPTS% -XX:MetaspaceSize=256m
+set JAVA_OPTS=%JAVA_OPTS% -XX:MaxMetaspaceSize=256m
+
+set JAVA_OPTS=%JAVA_OPTS% -Xloggc:%CATALINA_HOME%\logs\gc.log
+set JAVA_OPTS=%JAVA_OPTS% -XX:+UseConcMarkSweepGC
+set JAVA_OPTS=%JAVA_OPTS% -XX:+UseCMSCompactAtFullCollection
+set JAVA_OPTS=%JAVA_OPTS% -XX:+CMSClassUnloadingEnabled
+set JAVA_OPTS=%JAVA_OPTS% -XX:+PrintHeapAtGC
+set JAVA_OPTS=%JAVA_OPTS% -XX:+PrintGCDetails
+set JAVA_OPTS=%JAVA_OPTS% -XX:+PrintGCDateStamps
+set JAVA_OPTS=%JAVA_OPTS% -XX:+UseGCLogFileRotation
+set JAVA_OPTS=%JAVA_OPTS% -XX:NumberOfGCLogFiles=5
+set JAVA_OPTS=%JAVA_OPTS% -XX:GCLogFileSize=10M
+set JAVA_OPTS=%JAVA_OPTS% -XX:+HeapDumpOnOutOfMemoryError
+set JAVA_OPTS=%JAVA_OPTS% -XX:HeapDumpPath=%CATALINA_HOME%\logs\
+set JAVA_OPTS=%JAVA_OPTS% -XX:ErrorFile=%CATALINA_HOME%\logs\hs_err_pid%p.log
+
+REM For commercial use when using Oracle Java
+REM set JAVA_OPTS=%JAVA_OPTS% -XX:+UnlockCommercialFeatures
+REM set JAVA_OPTS=%JAVA_OPTS% -XX:+FlightRecorder
+
+REM set JAVA_OPTS=%JAVA_OPTS% -Dsun.net.client.defaultReadTimeout=60000
+REM set JAVA_OPTS=%JAVA_OPTS% -Dsun.net.client.defaultConnectTimeout=30000
+REM set JAVA_OPTS=%JAVA_OPTS% -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2
+
 set LANG=en_US
