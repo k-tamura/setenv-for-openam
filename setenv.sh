c0f3b8d0825d5e312fef1865f79f0eb63d27b63c
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-01-31 18:25:29 +0900

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -9,6 +9,10 @@ JAVA_OPTS="$JAVA_OPTS -Xmx2048m"
 JAVA_OPTS="$JAVA_OPTS -XX:MetaspaceSize=256m"
 JAVA_OPTS="$JAVA_OPTS -XX:MaxMetaspaceSize=256m"
 
+JAVA_OPTS="$JAVA_OPTS -Dsun.net.client.defaultReadTimeout=60000"
+JAVA_OPTS="$JAVA_OPTS -Dsun.net.client.defaultConnectTimeout=30000"
+JAVA_OPTS="$JAVA_OPTS -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2"
+
 JAVA_OPTS="$JAVA_OPTS -Xloggc:$CATALINA_HOME/logs/gc.log"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSCompactAtFullCollection"
@@ -19,6 +23,7 @@ JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDateStamps"
 JAVA_OPTS="$JAVA_OPTS -XX:+UseGCLogFileRotation"
 JAVA_OPTS="$JAVA_OPTS -XX:NumberOfGCLogFiles=5"
 JAVA_OPTS="$JAVA_OPTS -XX:GCLogFileSize=10M"
+
 JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
 JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$CATALINA_HOME/logs/"
 JAVA_OPTS="$JAVA_OPTS -XX:ErrorFile=$CATALINA_HOME/logs/hs_err_pid%p.log"
@@ -26,9 +31,5 @@ JAVA_OPTS="$JAVA_OPTS -XX:ErrorFile=$CATALINA_HOME/logs/hs_err_pid%p.log"
 # For commercial use when using Oracle Java
 # JAVA_OPTS="$JAVA_OPTS -XX:+UnlockCommercialFeatures"
 # JAVA_OPTS="$JAVA_OPTS -XX:+FlightRecorder"
-
-# JAVA_OPTS="$JAVA_OPTS -Dsun.net.client.defaultReadTimeout=60000"
-# JAVA_OPTS="$JAVA_OPTS -Dsun.net.client.defaultConnectTimeout=30000"
-# JAVA_OPTS="$JAVA_OPTS -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2"
 
 LANG="en_US"
